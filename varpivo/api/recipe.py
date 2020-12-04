4918a7dea563170586882317a8bdb386f3530e60
MatusPilnan <m.pilnan@gmail.com>
2020-12-04 14:35:32 +0100

    add doc stuff

--- varpivo/api/recipe.py
+++ varpivo/api/recipe.py
@@ -15,6 +15,7 @@ from varpivo.utils import Event
 @app.route("/recipe")
 class RecipeList(Resource):
     @app.response(HTTPStatus.OK, description="", validator=app.create_validator('recipe_list', recipe_list_model))
+    @app.doc(tags=['Recipes'])
     async def get(self):
         '''Retrieve all available recipes
 
@@ -37,22 +38,24 @@ recipe_steps = app.create_validator('recipe_steps', recipe_steps_model)
 @app.param("recipeId", "Recipe ID", "path", required=True)
 @app.route('/recipe/<recipeId>')
 class Recipe(Resource):
-
+    @app.doc(tags=['Recipes'])
     @app.response(HTTPStatus.NOT_FOUND, "Recipe not found")
     @app.response(HTTPStatus.OK, description="Returns a signle recipe",
                   validator=app.create_validator('recipe', recipe_model))
     async def get(self, recipeId):
+        """Get single recipe"""
         try:
             return jsonify(CookBook.get_instance()[recipeId].cookbook_entry)
         except KeyError:
             return jsonify({"error": 'Recipe not found'}), HTTPStatus.NOT_FOUND
 
+    @app.doc(tags=['Recipes'])
     @app.response(HTTPStatus.NOT_FOUND, "Recipe not found")
     @app.response(HTTPStatus.CONFLICT, "Recipe already selected")
     @app.response(HTTPStatus.OK, description="",
                   validator=recipe_steps)
     async def post(self, recipeId):
-
+        """Select recipe and start brew session"""
         try:
             if CookBook.get_instance().select_recipe(recipeId):
                 return jsonify({"steps": list(map(step_to_dict, CookBook.get_instance()[recipeId].steps_list))})
@@ -67,7 +70,9 @@ class Recipe(Resource):
 @app.route('/step/<stepId>')
 class StepStart(Resource):
     @app.response(HTTPStatus.OK, description="", validator=recipe_step)
+    @app.doc(tags=['Recipe steps'])
     async def post(self, stepId):
+        """Start specified step"""
         if not CookBook.get_instance().selected_recipe:
             return jsonify({"error": 'No recipe selected'}), HTTPStatus.FAILED_DEPENDENCY
         try:
@@ -80,7 +85,9 @@ class StepStart(Resource):
         return jsonify(step_to_dict(step))
 
     @app.response(HTTPStatus.OK, description="", validator=recipe_step)
+    @app.doc(tags=['Recipe steps'])
     async def delete(self, stepId):
+        """Finish specified step"""
         if not CookBook.get_instance().selected_recipe:
             return jsonify({"error": 'No recipe selected'}), HTTPStatus.FAILED_DEPENDENCY
         try:
@@ -98,12 +105,15 @@ class StepStart(Resource):
 @app.route("/status")
 class BrewStatus(Resource):
     @app.response(HTTPStatus.OK, description='OK', validator=app.create_validator('brew_session', brew_session_model))
+    @app.doc(tags=['Brew session status'])
     async def get(self):
+        """Get currently selected recipe with steps"""
         if not CookBook.get_instance().selected_recipe:
             return jsonify({"error": 'No recipe selected'}), HTTPStatus.FAILED_DEPENDENCY
         recipe = CookBook.get_instance().selected_recipe
         return jsonify({"recipe": recipe.cookbook_entry, "steps": list(map(step_to_dict, recipe.steps_list))})
 
+    @app.doc(tags=['Brew session status'])
     async def delete(self):
         """Reset state - unselect any selected recipe"""
         CookBook.get_instance().unselect_recipe()
@@ -113,6 +123,7 @@ class BrewStatus(Resource):
 @app.route("/scale")
 class ScaleRes(Resource):
     @app.param('grams', description='Real weight used for calibration', required=True, schema={"type": "integer"})
+    @app.doc(tags=['Scale'])
     async def patch(self):
         """Start scale calibration"""
         weight = request.args['grams']
@@ -120,6 +131,7 @@ class ScaleRes(Resource):
         await event_queue.put(Event(Event.WS, payload=json.dumps({"content": "calibration", "payload": "ready"})))
         return jsonify({}), HTTPStatus.NO_CONTENT
 
+    @app.doc(tags=['Scale'])
     async def put(self):
         """Find scale reference units, after weight was PUT on the scale"""
         if not Scale.get_instance().calibrating:
@@ -127,6 +139,7 @@ class ScaleRes(Resource):
         await event_queue.put(Event(Event.CALIBRATION_READY, payload=None))
         return jsonify({}), HTTPStatus.NO_CONTENT
 
+    @app.doc(tags=['Scale'])
     async def delete(self):
         """Tare the scale"""
         Scale.get_instance().tare()
@@ -136,10 +149,14 @@ class ScaleRes(Resource):
 @app.route("/brizolit/je/cesta/neprestrelna/vesta")
 class WebSocketKeg(Resource):
     @app.response(HTTPStatus.OK, description="Tatrofkaaaaaa", validator=app.create_validator('wskeg', ws_message_model))
+    @app.doc(tags=['WS'])
     def get(self):
+        """Resource format for WS messages"""
         return jsonify({""}, HTTPStatus.IM_A_TEAPOT)
 
     @app.response(HTTPStatus.OK, description="ZonickaaaAAA",
                   validator=app.create_validator('temperature', ws_temperature_model))
+    @app.doc(tags=['WS'])
     def post(self):
+        """Format of WS message with temperature"""
         return jsonify({""}, HTTPStatus.IM_A_TEAPOT)
