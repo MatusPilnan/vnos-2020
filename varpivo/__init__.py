e6e02d120a73e1b8487db58fe20b0d4b80ea1be1
MatusPilnan <m.pilnan@gmail.com>
2021-01-04 14:55:56 +0100

    add observable system info class

--- varpivo/__init__.py
+++ varpivo/__init__.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 from asyncio import Queue
 from functools import wraps
 
@@ -13,6 +12,7 @@ from varpivo.hardware.display import Display
 from varpivo.hardware.heater import Heater
 from varpivo.hardware.scale import Scale
 from varpivo.hardware.thermometer import Thermometer
+from varpivo.info.system_info import SystemInfo
 from varpivo.utils import Event
 
 app = Pint(__name__, title="Var:Pivo API")
@@ -54,9 +54,9 @@ def collect_websocket(func):
 @app.websocket('/tap')
 @collect_websocket
 async def ws(queue):
-    _, message = await weight_to_keg()
+    _, message = await SystemInfo.weight_to_keg()
     await websocket.send(message)
-    _, _, message = await temperature_to_keg()
+    _, _, message = await SystemInfo.temperature_to_keg()
     await websocket.send(message)
     while True:
         data = await queue.get()
@@ -69,39 +69,13 @@ async def broadcast(message):
 
 
 async def send_weight():
-    last_reading = None
-    while True:
-        weight, message = await weight_to_keg()
-        if last_reading != weight:
-            Display.get_instance().weight = weight
-            await broadcast(message)
-            last_reading = weight
-        await asyncio.sleep(0.5)
-
-
-async def weight_to_keg():
-    weight = int(await Scale.get_instance().weight)
-    return weight, json.dumps({"payload": json.dumps(weight), "content": "weight"})
+    weight, message = await SystemInfo.weight_to_keg()
+    await broadcast(message)
 
 
 async def send_temperature():
-    last_reading = None
-    while True:
-        temperature, heating, message = await temperature_to_keg()
-        if (temperature, heating) != last_reading:
-            Display.get_instance().temperature = temperature
-            Display.get_instance().heating = heating
-            await broadcast(message)
-            last_reading = (temperature, heating)
-        await asyncio.sleep(0.5)
-
-
-async def temperature_to_keg():
-    temperature = round(await Thermometer.get_instance().temperature)
-    heating = Heater.get_instance().heat
-    message = json.dumps({"payload": json.dumps({"temperature": temperature, "heating": heating}),
-                          "content": "temperature"})
-    return temperature, heating, message
+    temperature, heating, message = await SystemInfo.temperature_to_keg()
+    await broadcast(message)
 
 
 async def observe():
@@ -124,8 +98,9 @@ async def shutdown():
 from varpivo.api import recipe
 from quart_openapi import OpenApiView
 
-asyncio.ensure_future(send_weight())
-asyncio.ensure_future(send_temperature())
+SystemInfo.add_observer(send_temperature, [SystemInfo.TEMPERATURE, SystemInfo.HEATING])
+SystemInfo.add_observer(send_weight, [SystemInfo.WEIGHT])
+asyncio.ensure_future(SystemInfo.collect_info())
 asyncio.ensure_future(observe())
 
 
