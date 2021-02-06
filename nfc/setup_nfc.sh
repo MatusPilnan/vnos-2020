7329b513019d4b505083c9c0cbb2a31dff977c26
MatusPilnan <m.pilnan@gmail.com>
2021-02-06 18:37:23 +0100

    change NFC setup to install libnfc from raspbian repo

--- nfc/setup_nfc.sh
+++ nfc/setup_nfc.sh
@@ -4,15 +4,4 @@ sudo mkdir /etc/nfc
 cp libnfc.conf /etc/nfc/libnfc.conf
 
 sudo apt-get update
-sudo apt-get install libusb-dev libpcsclite-dev i2c-tools
-
-cd ~
-wget http://dl.bintray.com/nfc-tools/sources/libnfc-1.7.1.tar.bz2
-tar -xf libnfc-1.7.1.tar.bz2
-
-cd libnfc-1.7.1
-./configure --prefix=/usr --sysconfdir=/etc
-make
-sudo make install
-
-# http://wiki.sunfounder.cc/index.php?title=PN532_NFC_Module_for_Raspberry_Pi
+sudo apt-get install libusb-dev libpcsclite-dev i2c-tools libnfc-examples
